8b03d38d09cd2fe116f48d210e0b3d3359d0259e
mwachala3000 <paulmwachala@gmail.com>
2020-06-13 22:15:50 +0300

    Refactor js functions to be cleaner, get request returns dictionary of status over time

--- bin/static/main.js
+++ bin/static/main.js
@@ -1,29 +1,53 @@
 $('document').ready( () => {
-    var get_live_country = () => {
-        fetch(`https://api.covid19api.com/stats`)
-        .then( response => {
-            response.json()
-            .then( data => {
-                return data
-            })
-        })
-        .catch( err => {
-            console.log(`error ndio hii\n\n\n***${err}`)
-        })
-    }
-    var chart = () => {
-        context = document.getElementById('graph').getContext('2d')
-        var chart = new Chart(context, {
+    $('#date_form').on('submit', (e) => {
+        e.preventDefault()
+        //todo
+        //get date, country, time span
+    })
+
+    chart = (cases, time) => {
+        //todo
+        //graph of deaths over time
+        //graph of infections over time
+        //graph of recoveries over time
+        let context = document.getElementById('graph').getContext('2d')
+        let chart = new Chart(context, {
             type: 'line',
+            labels:[...time],
             data: {
                 dataset:[{
                     label:'random',
-                    data:[1,2,3,4,5,6]
+                    data:[...cases]
                 }],
             }
         })
     }
 
-    chart();
+    get_request = (request, status) => {
+        let content = {'time':[], 'numbers':[]}
+        fetch(request)
+        .then( response => {
+            response.json()
+            .then( data => {
+                data.forEach(element => {
+                    content['time'].push(element['Date'])
+                    content['numbers'].push(element['Cases'])
+                });
+            })
+        })
+        return content
+    }
+
+    get_live_country = (country_name, days, status) => {
+        let days_as_milliseconds = days * 24 * 60 * 60 * 1000 
+        let x = new Date()
+        let y = new Date(x.getTime() - days_as_milliseconds)
+        stringify = dateobj => { return `${dateobj.toISOString().split('T')[0]}T00:00:00Z`}
+        let addr = `https://api.covid19api.com`
+        let request = `${addr}/total/country/${country_name}/status/${status}?from=${stringify(y)}&to=${stringify(x)}`
+        content = get_request(request)
+        return content
+    }
+    
 })
 
